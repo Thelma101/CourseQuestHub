49d5bff452c0389598a754a6224de056f24c4a45
DCTP-BE <akpata.thelma@gmail.com>
2024-04-28 19:02:07 +0100

    v2-auth-fix

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use('/api/cqh/courses-app', coursesApp);
 // Error handler middleware at the end
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Internal Server Error, Check Phone Number Input.');
+    res.status(500).send('Internal Server Error.');
 });
 
 const port = process.env.PORT || 3000;
